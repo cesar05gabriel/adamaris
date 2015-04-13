5a1d89afb1a586e9fbc6e67a294f68b9d105574e
= <=>
2015-04-13 01:11:02 -0400

    Sign In Activity - Sign in class implemented

--- src/app/src/main/java/edu/itesa/adamaris/android/activities/SignInActivity.java
+++ src/app/src/main/java/edu/itesa/adamaris/android/activities/SignInActivity.java
@@ -3,11 +3,21 @@ package edu.itesa.adamaris.android.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
+import edu.itesa.adamaris.android.GooglePlusSignIn.GooglePlusAuthentication;
 import edu.itesa.adamaris.android.R;
 import edu.itesa.adamaris.android.fragments.SignInFragment;
 
-public class SignInActivity extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import android.view.View.OnClickListener;
+
+public class SignInActivity extends ActionBarActivity implements ConnectionCallbacks,
+        OnConnectionFailedListener, OnClickListener{
+
+    GooglePlusAuthentication googlePlusAuthentication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,21 @@ public class SignInActivity extends ActionBarActivity {
                     .commit();
         }
 
+        googlePlusAuthentication =
+                new GooglePlusAuthentication(this,
+                this,
+                this);
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+        googlePlusAuthentication.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        googlePlusAuthentication.disconnect();
     }
 
     @Override
@@ -29,5 +54,32 @@ public class SignInActivity extends ActionBarActivity {
         actionBar.hide();
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                // Signin button clicked
+                googlePlusAuthentication.logIn(this);
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        googlePlusAuthentication.onConnected(bundle);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        googlePlusAuthentication.connect();
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        googlePlusAuthentication.onConnectionFailed(connectionResult, this);
     }
 }
