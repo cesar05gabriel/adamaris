d2e5127a3a94f505f3a379f76422e70a0f7956e5
= <=>
2015-04-25 12:02:16 +0200

    Activity Main

--- src/app/src/main/java/edu/itesa/adamaris/android/activities/Personal_Agenda.java
+++ src/app/src/main/java/edu/itesa/adamaris/android/activities/Personal_Agenda.java
@@ -1,44 +1,37 @@
-package edu.itesa.adamaris.android;
+package edu.itesa.adamaris.android.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.content.Intent;
 
-import edu.itesa.adamaris.android.activities.Personal_Agenda;
+import edu.itesa.adamaris.android.MainActivity;
+import edu.itesa.adamaris.android.R;
 
-
-public class MainActivity extends Activity {
+public class Personal_Agenda extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_personal__agenda);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_personal__agenda, menu);
         return true;
     }
 
     public void mostrar(View view)
     {
-        Intent intent = new Intent(this, Personal_Agenda.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -53,21 +46,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 }
